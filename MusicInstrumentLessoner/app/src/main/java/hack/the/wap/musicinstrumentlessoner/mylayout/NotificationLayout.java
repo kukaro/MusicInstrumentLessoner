b375c40441d4f52c4d7da27aa4b15a440c0d7cbf
kukaro <justkukaro@naver.com>
2018-07-22 08:08:43 +0900

    v0.2.11 change notification fragment

--- MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/NotificationLayout.java
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/NotificationLayout.java
@@ -1,9 +1,11 @@
-package hack.the.wap.musicinstrumentlessoner;
+package hack.the.wap.musicinstrumentlessoner.mylayout;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+
+import hack.the.wap.musicinstrumentlessoner.R;
 
 /*
 참고 사이트 : https://medium.com/@douglas.iacovelli/the-beauty-of-custom-views-and-how-to-do-it-79c7d78e2088
@@ -12,7 +14,7 @@ import android.widget.LinearLayout;
 public class NotificationLayout extends LinearLayout {
 
     {
-        inflate(getContext(),R.layout.notification_layout,this);
+        inflate(getContext(), R.layout.notification_layout,this);
     }
 
     public NotificationLayout(Context context) {
