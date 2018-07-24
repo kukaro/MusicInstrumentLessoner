0346a3aec1595e84341cbd77465c3d99d33233d9
kukaro <justkukaro@naver.com>
2018-07-25 08:06:45 +0900

    v0.2.18 edit notification layout template

--- MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/NotificationLayout.java
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/NotificationLayout.java
@@ -1,9 +1,12 @@
 package hack.the.wap.musicinstrumentlessoner.mylayout;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import hack.the.wap.musicinstrumentlessoner.R;
 
@@ -13,9 +16,14 @@ import hack.the.wap.musicinstrumentlessoner.R;
  */
 
 public class NotificationLayout extends LinearLayout {
+    private TextView tvNotificationLayName;
+    private TextView tvNotificationLayDate;
+    private TextView tvNotificationLayMain;
+    private TextView tvNotificationLayMusicTitle;
+
 
     {
-        inflate(getContext(), R.layout.notification_layout,this);
+        initView();
     }
 
     public NotificationLayout(Context context) {
@@ -24,6 +32,7 @@ public class NotificationLayout extends LinearLayout {
 
     public NotificationLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        getAttrs(attrs);
     }
 
     public NotificationLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -32,5 +41,36 @@ public class NotificationLayout extends LinearLayout {
 
     public NotificationLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initView() {
+        inflate(getContext(), R.layout.notification_layout, this);
+        tvNotificationLayName = findViewById(R.id.tvNotificationLayName);
+        tvNotificationLayDate = findViewById(R.id.tvNotificationLayDate);
+        tvNotificationLayMain = findViewById(R.id.tvNotificationLayMain);
+        tvNotificationLayMusicTitle = findViewById(R.id.tvNotificationLayMusicTitle);
+    }
+
+    private void getAttrs(AttributeSet attributeSet) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attributeSet, R.styleable.NotificationLayout);
+        Log.e("TAG", "getAttrs: "+typedArray);
+        setTypeArray(typedArray);
+    }
+
+    private void getAttrs(AttributeSet attributeSet, int defStyle) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attributeSet, R.styleable.NotificationLayout, defStyle, 0);
+        setTypeArray(typedArray);
+    }
+
+    private void setTypeArray(TypedArray typedArray){
+        String notification_lay_name = typedArray.getString(R.styleable.NotificationLayout_notification_lay_name);
+        String notification_lay_date = typedArray.getString(R.styleable.NotificationLayout_notification_lay_date);
+        String notification_lay_main = typedArray.getString(R.styleable.NotificationLayout_notification_lay_main);
+        String notification_lay_music_title = typedArray.getString(R.styleable.NotificationLayout_notification_lay_music_title);
+        tvNotificationLayName.setText(notification_lay_name);
+        tvNotificationLayDate.setText(notification_lay_date);
+        tvNotificationLayMain.setText(notification_lay_main);
+        tvNotificationLayMusicTitle.setText(notification_lay_music_title);
+        typedArray.recycle();
     }
 }
