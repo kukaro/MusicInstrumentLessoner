eca7edeae5b3404605e20be35afb3b134e31add5
kukaro <justkukaro@naver.com>
2018-07-28 11:31:59 +0900

    v0.3.2 Make Template Detail Activity

---
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/TemplateDetailActivity.java
@@ -0,0 +1,13 @@
+package hack.the.wap.musicinstrumentlessoner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemplateDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template_detail);
+    }
+}
