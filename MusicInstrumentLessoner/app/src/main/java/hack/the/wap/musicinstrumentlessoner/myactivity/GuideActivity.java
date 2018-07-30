5ea88185511bf75605403b88577cfc6840fdbb05
kukaro <justkukaro@naver.com>
2018-07-31 05:29:06 +0900

    v0.5.4 connect GuideActivity viewSetValue

--- MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/GuideActivity.java
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/GuideActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import hack.the.wap.musicinstrumentlessoner.R;
+import hack.the.wap.musicinstrumentlessoner.debug.DebugImageMatch;
 import hack.the.wap.musicinstrumentlessoner.model.dto.TemplateDto;
 import hack.the.wap.musicinstrumentlessoner.session.Session;
 
@@ -15,6 +17,10 @@ public class GuideActivity extends AppCompatActivity {
     private static Session session;
     private TemplateDto mainTemplate;
     private ImageView ivGuideActLeftArrow;
+    private ImageView ivGuideActTeacher;
+    private ImageView ivGuideActMusician;
+    private TextView tvGuideActTeacherName;
+    private TextView tvGuideActMusicName;
 
     {
         session = Session.getInstance();
@@ -25,14 +31,19 @@ public class GuideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
         ivGuideActLeftArrow = findViewById(R.id.ivGuideActLeftArrow);
+        ivGuideActTeacher = findViewById(R.id.ivGuideActTeacher);
+        ivGuideActMusician = findViewById(R.id.ivGuideActMusician);
+        tvGuideActTeacherName = findViewById(R.id.tvGuideActTeacherName);
+        tvGuideActMusicName = findViewById(R.id.tvGuideActMusicName);
 
         instance = this;
 
         Intent intent = getIntent();
-        mainTemplate = (TemplateDto) intent.getSerializableExtra("data");
+        mainTemplate = (TemplateDto) intent.getSerializableExtra("main");
         Log.e("SAFE", "onCreate >>> " + mainTemplate);
 
         viewSetListener();
+        viewSetValue();
     }
 
     private void viewSetListener() {
@@ -41,7 +52,14 @@ public class GuideActivity extends AppCompatActivity {
         });
     }
 
-    public GuideActivity getInstance(){
+    private void viewSetValue() {
+        ivGuideActTeacher.setImageResource(DebugImageMatch.getImageFromName(mainTemplate.getOwner().getName()));
+        ivGuideActMusician.setImageResource(DebugImageMatch.getImageFromName(mainTemplate.getMusician()));
+        tvGuideActTeacherName.setText(mainTemplate.getOwner().getName());
+        tvGuideActMusicName.setText(mainTemplate.getMusicTitle());
+    }
+
+    public GuideActivity getInstance() {
         return instance;
     }
 }
