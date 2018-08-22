2019e77594102e5849def0a862561db9e9e4abc3
kukaro <justkukaro@naver.com>
2018-08-23 07:08:41 +0900

    v0.7.11 complete debug act

--- MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/UserImageLayout.java
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/mylayout/UserImageLayout.java
@@ -1,6 +1,7 @@
 package hack.the.wap.musicinstrumentlessoner.mylayout;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -8,6 +9,9 @@ import android.widget.TextView;
 import hack.the.wap.musicinstrumentlessoner.R;
 import hack.the.wap.musicinstrumentlessoner.debug.DebugImageMatch;
 import hack.the.wap.musicinstrumentlessoner.model.dto.UserDto;
+import hack.the.wap.musicinstrumentlessoner.myactivity.DebugActivity;
+import hack.the.wap.musicinstrumentlessoner.myactivity.MainActivity;
+import hack.the.wap.musicinstrumentlessoner.myactivity.UserGroupDetailActivity;
 
 public class UserImageLayout extends ImageLayout {
     private UserDto mainUser;
@@ -19,6 +23,7 @@ public class UserImageLayout extends ImageLayout {
         tvMyImageName = findViewById(R.id.tvMyImageName);
         tvMyImageEmail = findViewById(R.id.tvMyImageEmail);
         setImage();
+        setListener();
     }
 
     public UserImageLayout(Context context, @Nullable AttributeSet attrs) {
@@ -37,5 +42,12 @@ public class UserImageLayout extends ImageLayout {
         ivMyImage.setImageResource(DebugImageMatch.getImageFromName(mainUser.getName()));
         tvMyImageName.setText(mainUser.getName());
         tvMyImageEmail.setText(mainUser.getEmail());
+    }
+
+    public void setListener(){
+        setOnClickListener((v)->{
+            Intent intent = new Intent(getContext(), DebugActivity.class);
+            getContext().startActivity(intent);
+        });
     }
 }
