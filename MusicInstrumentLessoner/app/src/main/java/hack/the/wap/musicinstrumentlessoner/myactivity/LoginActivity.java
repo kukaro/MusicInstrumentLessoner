8852ae8766698381747687e2702f09c74317ad85
kukaro <justkukaro@naver.com>
2018-10-15 08:26:00 +0900

    asdf

--- MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/LoginActivity.java
+++ MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/LoginActivity.java
@@ -3,9 +3,11 @@ package hack.the.wap.musicinstrumentlessoner.myactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -19,6 +21,11 @@ import hack.the.wap.musicinstrumentlessoner.model.dto.TemplatePracticeDto;
 import hack.the.wap.musicinstrumentlessoner.model.dto.UserDto;
 import hack.the.wap.musicinstrumentlessoner.model.dto.UserGroupDto;
 import hack.the.wap.musicinstrumentlessoner.session.Session;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
     private static LoginActivity instance;
@@ -26,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     private static EditText etEmail;
     private static EditText etPassword;
     private static Session session;
+    //    private static OkHttpClient client = new OkHttpClient.Builder().addNetworkInterceptor(new LogInterceptor()).build();
+    private static OkHttpClient client = new OkHttpClient();
 
     /*
      init block
@@ -41,6 +50,21 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         ivLogin = findViewById(R.id.ivLogin);
         ivLogin.setOnClickListener(v -> {
+            Request request = new Request.Builder()
+                    .url("http://localhost:4000")
+                    .get()
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Log.e("TAG", "onResponse: success");
+                }
+            });
             String name = loginProcess(etEmail.getText().toString(), etPassword.getText().toString());
             if (name != null) {
                 Intent intent = new Intent(LoginActivity.getInstance(), MainActivity.class);
